5640f29381e31ee59a7cc8348add1f713c1936fd
g3mhunt3r <rgopikrishnan28@gmail.com>
2020-05-09 20:44:43 +0530

    implemet function to add 2 randomly

--- 2048.py
+++ 2048.py
@@ -1,3 +1,5 @@
+import random
+
 def emp(T):
     for r in T:
         for c in r:
@@ -7,5 +9,15 @@ def emp(T):
                 print(c,end = "\t")
         print("\n\n")
 
+def add(T):
+    while True:
+        x=random.randint(0,3)
+        y=random.randint(0,3)
+        if T[x][y]==0:
+            T[x][y]=2
+            break
+
 T = [[2048,2,4,8],[0]*4,[0]*4,[0]*4]
+emp(T)
+add(T)
 emp(T)
